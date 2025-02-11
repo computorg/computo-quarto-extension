7bf92c0ae2a7d571a2eee0d81d8c725afa2e56c1
François-David Collin <Francois-David.Collin@umontpellier.fr>
2025-02-11 11:45:10 +0100

    tlmgr hack for pdflatex call from diagram.lua

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,2 +1,6 @@
 sudo apt-get install -y inkscape
 quarto install chromium
+mkdir -p ~/.local/bin
+~/.TinyTeX/bin/x86_64-linux/tlmgr option sys_bin ~/.local/bin
+~/.TinyTeX/bin/x86_64-linux/tlmgr path add
+~/.TinyTeX/bin/x86_64-linux/tlmgr update --self
