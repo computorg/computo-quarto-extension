30a3a2581db42b28995b41f99ec4805261fe645a
Julien Chiquet <julien.chiquet@inrae.fr>
2026-06-30 13:48:47 +0200

    update sys dependencies: ipython3  +lighter chrome quarto install

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,6 +1,7 @@
-sudo apt-get install -y inkscape
-quarto install chromium
-mkdir -p ~/.local/bin
-~/.TinyTeX/bin/x86_64-linux/tlmgr option sys_bin ~/.local/bin
-~/.TinyTeX/bin/x86_64-linux/tlmgr path add
-~/.TinyTeX/bin/x86_64-linux/tlmgr update --self
+sudo apt-get install -y inkscape ipython3
+quarto install chrome-headless-shell
+
+# mkdir -p ~/.local/bin
+# ~/.TinyTeX/bin/x86_64-linux/tlmgr option sys_bin ~/.local/bin
+# ~/.TinyTeX/bin/x86_64-linux/tlmgr path add
+# ~/.TinyTeX/bin/x86_64-linux/tlmgr update --self
